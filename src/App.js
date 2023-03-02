e0ce32dda357296d6ad2b1ee0fce087c1b68c908
Acris <nicolas.velez99@hotmail.com>
2023-03-02 12:25:00 -0500

    [init]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DataTable from "react-data-table-component";
+import Navbar from "./pages/layout/Navbar/Navbar";
+
+const data = [
+  {
+    id: 1,
+    name: "Nicolas Velez",
+    age: 12,
+  },
+  {
+    id: 2,
+    name: "Nicolas Velez",
+    age: 12,
+  },
+  {
+    id: 3,
+    name: "Nicolas Velez",
+    age: 12,
+  },
+];
+
+const columns = [
+  {
+    name: "NAME",
+    selector: (row) => row.name,
+  },
+  {
+    name: "NAME",
+    selector: (row) => row.age,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <DataTable
+        columns={columns}
+        data = {data}
+      />
     </div>
   );
 }
