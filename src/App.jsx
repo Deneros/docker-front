b120295066926ec821aa9835ecb0986e1b79b023
Acris <nicolas.velez99@hotmail.com>
2023-03-11 09:51:48 -0500

    [feat]: se agrega rutas

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Document from "./pages/documents/Document";
+// import Home from "./pages/home/Home"
+
+
+
+function App() {
+  return (
+    <div className="App">    
+      <Routes>
+        <Route element={<Document />} path="/document" />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
