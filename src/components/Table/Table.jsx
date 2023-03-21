869ed762452e0942f9a5d9a98e89c5fddea0adca
Acris <nicolas.velez99@hotmail.com>
2023-03-21 17:31:33 -0500

    [feat]: se agrega metodos para poder usar el filtro del input

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,104 +1,160 @@
 import React from "react";
-import { styled, Container, css } from "@nextui-org/react";
+import { styled } from "@nextui-org/react";
 import DataTable from "react-data-table-component";
-import './Table.css';
+import InputFilter from "../../components/Input/InputFilter";
+// import "./Table.css";
+
+// const StyledTable = styled(DataTable, {
+//   ".rdt_Table": {
+//   },
+//   ".rdt_TableHeader":{
+//     marginTop:"6% !important",
+//   }
+// })
+
+const StyledContainerDiv = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  padding: "0 !important",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "#555555",
+  flexWrap: "wrap",
+});
+
+const StyledContainerTable = styled("div", {
+  // padding:"0 !important",
+  display: "flex !important",
+  position: "relative",
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: "20px",
+  width: "92vw",
+  height: "70vh",
+  backgroundColor: "white",
+  borderRadius: "5px",
+  flexWrap: "wrap",
+});
+
+const StyledCointanerList = styled("div", {
+  position: "absolute",
+  top: "0",
+  width: "100%",
+  height: "8vh",
+  backgroundColor: "#DBDBDB",
+});
+
+const StyledUl = styled("ul", {
+  padding: "0",
+  margin: "0",
+  display: "flex",
+  alignItems: "center",
+  height: "8vh",
+  ".active": {
+    position: "abolute",
+    cursor: "default",
+    backgroundColor: "white",
+    borderTopLeftRadius: "3px",
+    borderTopRightRadius: "3px",
+  },
+});
+
+const StyledLi = styled("li", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  listStyle: "none",
+  borderRight: "solid 0.5px gray",
+  boxSizing: "border-box",
+  padding: "0px 50px",
+  height: "100%",
+  zIndex: "2",
+  "&:hover": {
+    cursor: "pointer",
+  },
+  "&:last-child": {
+    cursor: "default",
+    border: "none",
+    width: "100%",
+    backgroundColor: "#555555",
+  },
+
+  ".active": {
+    cursor: "default",
+    backgroundColor: "white",
+  },
+});
+
+const Styleda = styled("a", {
+  display: "flex",
+  alignItems: "center",
+  textDecoration: "none",
+  fontSize: "16px",
+});
 
 function Table(props) {
 
-  const StyledContainerTable = styled("div", {
-    // padding:"0 !important",
-    display: "flex !important",
-    position: "relative",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "20px",
-    width: "92vw",
-    height: "70vh",
-    backgroundColor: "white",
-    borderRadius: "5px",
-    flexWrap: "wrap",
-  });
+  const [filterText, setFilterText] = React.useState("");
+  const [resetPaginationToggle, setResetPaginationToggle] =
+    React.useState(false);
 
-  const StyledCointanerList = styled("div", {
-    position: "absolute",
-    top: "0",
-    width: "100%",
-    height: "8vh",
-    backgroundColor: "#DBDBDB",
-  });
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText("");
+      }
+    };
 
-  const StyledUl = styled("ul", {
-    padding: "0",
-    margin: "0",
-    display: "flex",
-    alignItems: "center",
-    height: "8vh",
-    ".active":{
-      position:"abolute",
-      cursor:"default",
-      backgroundColor:"white",
-      borderTopLeftRadius:"3px",
-      borderTopRightRadius:"3px"
-    }
-  });
+    return (
+      <InputFilter
+        onFilter={(e) => setFilterText(e.target.value)}
+        onClear={handleClear}
+        filterText={filterText}
+      />
+    );
+  }, [filterText, resetPaginationToggle]);
 
-  const StyledLi = styled("li", {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    listStyle: "none",
-    borderRight: "solid 0.5px gray",
-    boxSizing: "border-box",
-    padding: "0px 50px",
-    height: "100%",
-    zIndex:"2",
-    "&:hover": {
-      cursor: "pointer",
-    },
-    "&:last-child": {
-      cursor:"default",
-      border: "none",
-      width:"100%",
-      backgroundColor: "#555555"
-    },
-
-    ".active":{
-      cursor:"default",
-      backgroundColor:"white"
-    }
-  });
-
-  const Styleda = styled("a", {
-    display: "flex",
-    alignItems: "center",
-    textDecoration: "none",
-    fontSize: "16px",
-  });
-
-  
-
-  
+  const filteredItems = props.data;
 
   return (
-    <StyledContainerTable>
-      <StyledCointanerList>
-        <StyledUl>
-          <StyledLi className="active">
-            <Styleda onClick={()=> props.onStateChange('usuario')}>Usuarios</Styleda>
-          </StyledLi>
-          <StyledLi>
-            <Styleda onClick={()=> props.onStateChange('documento')}>Documentos</Styleda>
-          </StyledLi>
-          <StyledLi>
-            <Styleda onClick={()=> props.onStateChange('consumo')}>Consumo</Styleda>
-          </StyledLi>
-          <StyledLi>
-            <Styleda></Styleda>
-          </StyledLi>
-        </StyledUl>
-      </StyledCointanerList>
-      <DataTable columns={props.columns} data={props.data} pagination />
-    </StyledContainerTable>
+    <StyledContainerDiv>
+      <StyledContainerTable>
+        <StyledCointanerList>
+          <StyledUl>
+            <StyledLi className="active">
+              <Styleda onClick={() => props.onStateChange("usuario")}>
+                Usuarios
+              </Styleda>
+            </StyledLi>
+            <StyledLi>
+              <Styleda onClick={() => props.onStateChange("documento")}>
+                Documentos
+              </Styleda>
+            </StyledLi>
+            <StyledLi>
+              <Styleda onClick={() => props.onStateChange("consumo")}>
+                Consumo
+              </Styleda>
+            </StyledLi>
+            <StyledLi>
+              <Styleda></Styleda>
+            </StyledLi>
+          </StyledUl>
+        </StyledCointanerList>
+        <div style={{ marginTop: "6%", position:"relative" }}>
+        <DataTable
+          columns={props.columns}
+          data={filteredItems}
+          pagination
+          paginationResetDefaultPage={resetPaginationToggle}
+          expandableRows
+          subHeader
+          subHeaderComponent={subHeaderComponentMemo}
+        />
+        </div>
+      </StyledContainerTable>
+    </StyledContainerDiv>
   );
 }
 
