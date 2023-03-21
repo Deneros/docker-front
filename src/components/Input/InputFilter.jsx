6c63ee5c73a7cc90771d13625fc58bba2e98b586
Acris <nicolas.velez99@hotmail.com>
2023-03-21 17:31:08 -0500

    [feat]: se agrega componente para el filtro en la tabla

---
+++ src/components/Input/InputFilter.jsx
@@ -0,0 +1,15 @@
+import { Input, Spacer } from "@nextui-org/react";
+
+ function InputFilter() {
+  return (
+    <>
+      <Input
+        clearable
+        bordered
+        labelPlaceholder="Busca"
+      />
+    </>
+  );
+}
+
+export default InputFilter;
