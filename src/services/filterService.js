f7150aaa50b59a0a8243cef254042a425b7108ae
Acris <nicolas.velez99@hotmail.com>
2023-04-14 08:29:09 -0500

    [feat]: nuevos filtros agregados

--- src/services/filterService.js
+++ src/services/filterService.js
@@ -1,9 +1,11 @@
 export const filterByUser = (data, filterText) => {
   return data.filter((item) => {
-    const textFilter = item.usu_nombre && item.usu_apelli ? 
-      item.usu_nombre.toLowerCase().includes(filterText.toLowerCase()) ||
-      item.usu_apelli.toLowerCase().includes(filterText.toLowerCase()) :true
-    return textFilter
+    const textFilter =
+      item.usu_nombre && item.usu_apelli
+        ? item.usu_nombre.toLowerCase().includes(filterText.toLowerCase()) ||
+          item.usu_apelli.toLowerCase().includes(filterText.toLowerCase())
+        : true;
+    return textFilter;
   });
 };
 
@@ -24,13 +26,51 @@ export const filterByDocument = (
       startDate && finishDate
         ? filterDate >= startDate && filterDate <= finishDate
         : true;
+
     const textFilter =
       item.document_name && item.state && item.destinataries
         ? item.document_name.toLowerCase().includes(filterText.toLowerCase()) ||
-          item.state.toLowerCase().includes(filterText.toLowerCase()) 
+          item.state.toLowerCase().includes(filterText.toLowerCase()) ||
+          item.destinataries.some((user) => {
+            return user.name.toLowerCase().includes(filterText.toLowerCase());
+          })
         : true;
 
     return dateFilter && textFilter;
+  });
+};
+
+export const filterByTransactions = (data, filterText) => {
+  return data.filter((item) => {
+    const idFilter = item.IdNumber
+      ? item.IdNumber.toLowerCase().includes(filterText.toLowerCase())
+      : true;
+    const nameFilter =
+      item.FirstName && item.FirstSurname && item.SecondSurname
+        ? item.FirstName.toLowerCase().includes(filterText.toLowerCase()) ||
+          item.FirstSurname.toLowerCase().includes(filterText.toLowerCase()) ||
+          item.SecondSurname.toLowerCase().includes(filterText.toLowerCase())
+        : true;
+    const transactionTypeFilter = item.TransactionTypeName
+      ? item.TransactionTypeName.toLowerCase().includes(
+          filterText.toLowerCase()
+        )
+      : true;
+
+    return idFilter || nameFilter || transactionTypeFilter;
+  });
+};
+
+export const filterByConsumption = (data, filterText) => {
+  return data.filter((item) => {
+    const boughtFilter = item.bought_firms
+      ? item.bought_firms.toString().includes(filterText)
+      : true;
+    const usedFilter = item.used_firms
+      ? item.used_firms.toString().includes(filterText)
+      : true;
+
+    return boughtFilter || usedFilter;
   });
 };
 
@@ -47,7 +87,14 @@ export const filterByTabs = (
   }
 
   if (tab === "documento") {
-    console.log(data);
     return filterByDocument(data, startDate, finishDate, filterText, typeDate);
+  }
+
+  if (tab === "consumo") {
+    return filterByConsumption(data, filterText);
+  }
+
+  if (tab === "transacciones") {
+    return filterByTransactions(data, filterText);
   }
 };
