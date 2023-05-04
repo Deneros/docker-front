b4b0a8d990d66990ac67e45be9c05442db3a11b7
Acris <nicolas.velez99@hotmail.com>
2023-05-04 16:24:05 -0500

    [feat]: implemtacion de modal

--- src/components/Expandable/ExpandableTableUser.jsx
+++ src/components/Expandable/ExpandableTableUser.jsx
@@ -1,55 +1,74 @@
-import { useEffect } from "react";
-import { Card, Text, Avatar, Grid, Spacer } from "@nextui-org/react";
-import { styled, Loading } from "@nextui-org/react";
+import { useState } from "react";
+import { Grid, Loading, Text } from "@nextui-org/react";
 import { useFetch } from "../../hooks/useFetch";
 import { URL } from "../../utils/constants";
 import GeneralCard from "../Cards/GeneralCard";
-
-const StyledCard = styled(Card, {
-  minWidth: "200px",
-  maxWidth: "200px",
-  margin: "0 10px",
-  textAlign: "center",
-});
+import GeneralModal from "../Modal/GeneralModal";
+import ModalContent from "../ModalContent/ModalContent";
 
 function ExpandableTableUser({ data }) {
+  const states = { completed: "Completados", pending: "Pendientes" };
+  
+  const [showModal, setShowModal] = useState(false);
+  const [state, setState] = useState("");
   const { data: userData, loading } = useFetch(
-    `${URL}user/${data.usu_id}/documents`
+    `${URL}user/${data.usu_id}/details`
   );
 
-  useEffect(() => {
-    console.log(userData);
-    console.log(data.usu_id);
-  }, []);
+  const handleClickCardCompleted = () => {
+    setShowModal(true);
+    setState("completed");
+  };
+
+  const handleClickCardPending = () => {
+    setShowModal(true);
+    setState("pending");
+  };
+
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
-    <Grid.Container gap={2} justify="center" alignItems="center">
-      <Grid xs={12} sm={6} md={3}>
-        <GeneralCard
-          title="Name"
-          value={`${data.usu_nombre} ${data.usu_apelli}`}
-        />
-      </Grid>
-      <Grid xs={12} sm={6} md={3}>
-        <GeneralCard
-          title="Total Documents"
-          value={loading ? <Loading /> : userData.total_documents}
-        />
-      </Grid>
-      <Grid xs={12} sm={6} md={3}>
-        <GeneralCard
-          title="Completed Documents"
-          value={loading ? <Loading /> : userData.completed_documents}
-          // onPress={(e)=>{console.log('hola')}}
-        />
-      </Grid>
-      <Grid xs={12} sm={6} md={3}>
-        <GeneralCard
-          title="Pending Documents"
-          value={loading ? <Loading /> : userData.pending_documents}
-        />
-      </Grid>
-    </Grid.Container>
+    <>
+      <Grid.Container gap={2} justify="center" alignItems="center">
+        <Grid xs={12} sm={6} md={3}>
+          <GeneralCard
+            title="Name"
+            value={`${data.usu_nombre} ${data.usu_apelli}`}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} md={3}>
+          <GeneralCard
+            title="Total Documents"
+            value={loading ? <Loading /> : userData.total_documents}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} md={3}>
+          <GeneralCard
+            title="Completed Documents"
+            value={loading ? <Loading /> : userData.completed_documents}
+            pressable={true}
+            onPress={handleClickCardCompleted}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} md={3}>
+          <GeneralCard
+            title="Pending Documents"
+            value={loading ? <Loading /> : userData.pending_documents}
+            pressable={true}
+            onPress={handleClickCardPending}
+          />
+        </Grid>
+      </Grid.Container>
+      <GeneralModal
+        title={"Documentos " + states[state]}
+        component={<ModalContent userId={data.usu_id} state={state} />}
+        visible={showModal}
+        onClose={closeModal}
+      />
+    </>
   );
 }
 
