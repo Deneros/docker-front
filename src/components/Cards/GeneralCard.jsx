3d07cec80ec009b4a451547f39a98dcaf6845119
Acris <nicolas.velez99@hotmail.com>
2023-05-04 16:18:59 -0500

    [refactor]: generalizar tarjeta

--- src/components/Cards/GeneralCard.jsx
+++ src/components/Cards/GeneralCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Card } from '@nextui-org/react';
 
-function GeneralCard({ title, value, cardCss={}, headerCSS={}, bodyCss={} }) {
+function GeneralCard({ title, value, cardCss={}, headerCSS={}, bodyCss={}, onPress, pressable }) {
   return (
-    <Card>
+    <Card isPressable={pressable} onPress={onPress}>
       <Card.Header style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{title}</Card.Header>
       <Card.Body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <h3>{value}</h3>
